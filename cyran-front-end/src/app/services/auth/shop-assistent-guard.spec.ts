24a41584f3770e1c629f96c9f868b4b1db8c09b0
abd alrahman saleh <mea.abdo.saleh@chatbotech>
2021-04-08 11:36:41 +0200

    move to public

--- cyran-front-end/src/app/services/auth/shop-assistent-guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ShopAssistentGuard } from './shop-assistent-guard';
-
-describe('ShopAssistentGuard', () => {
-  it('should create an instance', () => {
-    expect(new ShopAssistentGuard()).toBeTruthy();
-  });
-});
--- whois/config/db_properties.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = {
-	host: process.env.DB_HOST,
-	user: "postgres",
-	port: 5432,
-	password: "perdekj",
-	databaseName: "postgres"
-}
